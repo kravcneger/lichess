8f434edf75af7bcc40e031f7633f3daacd8f6f8c
kravcneger <kravcneger@gmail.com>
2021-05-03 17:38:23 +0400

    Add #OngoingGames

--- game.go
+++ game.go
@@ -41,3 +41,47 @@ func (c *Client) ExportGame(game_id string) (*Game, error) {
 	}
 	return game, err
 }
+
+func (c *Client) ExportOngoingGameOfUser(user_id string) (*Game, error) {
+	req, err := c.newRequest("GET", fmt.Sprintf("/api/user/%s/current-game", user_id), nil)
+
+	game := new(Game)
+	_, err = c.do(req, &game)
+	if err != nil {
+		return nil, err
+	}
+	return game, err
+}
+
+type OngoingGame struct {
+	FullId   string `json:"fullId"`
+	GameId   string `json:"gameId"`
+	Fen      string `json:"fen"`
+	Color    string `json:"color"`
+	LastMove string `json:"lastMove"`
+	Speed    string `json:"speed"`
+	Perf     string `json:"perf"`
+	Rated    bool   `json:"rated"`
+	Opponent struct {
+		Id       string `json:"id"`
+		Username string `json:"username"`
+		Rating   int    `json:"rating"`
+	}
+	IsMyTurn bool `json:"isMyTurn"`
+	//	GameState *GameState
+}
+
+type OngoingGames struct {
+	Games []OngoingGame `json:"NowPlaying"`
+}
+
+func (c *Client) OngoingGames() (*[]OngoingGame, error) {
+	req, err := c.newRequest("GET", "/api/account/playing", nil)
+
+	ongoingGames := new(OngoingGames)
+	_, err = c.do(req, &ongoingGames)
+	if err != nil {
+		return nil, err
+	}
+	return &ongoingGames.Games, nil
+}
